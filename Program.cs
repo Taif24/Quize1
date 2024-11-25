b1d68c7f65cae5c88756f626d1e6054a7497a3ed
Taif24 <taif69250@gmail.com>
2024-11-25 12:20:30 +0300

    Q6

--- Program.cs
+++ Program.cs
@@ -69,7 +69,7 @@ class Program2{
     }
 }*/
 //Q5
-class Program{
+/*class Program{
     static void Main(string[] args){
 int[] numbers = new int[20];
 for(int num = 1; num <= numbers.Length; num++){
@@ -80,5 +80,38 @@ while(num1 < 20){//I forget how to make the while loop:(
 
 }
 
+    }
+}*/
+//Q6
+class Program{
+    static void Main(string[] args){
+        Car obj1 = new Car();
+        obj1.Make = "Audi";
+        obj1.Model = "Geman";
+        obj1.Color = "Black";
+        obj1.Year = 2020;
+         Car obj2 = new Car();
+        obj2.Make = "BMW";
+        obj2.Model = "German";
+        obj2.Color = "Red";
+        obj2.Year = 2018;
+         Car obj3 = new Car();
+        obj3.Make = "Lexus";
+        obj3.Model = "Jabanese";
+        obj3.Color = "White";
+        obj3.Year = 2024;
+        obj1.DisplayDetails();
+        obj2.DisplayDetails();
+        obj3.DisplayDetails();
+    }
+}
+class Car{
+    public string Make {get; set;}
+    public string Model {get; set;}
+    public string Color {get; set;}
+    public int Year {get; set;}
+
+    public void DisplayDetails(){
+        Console.WriteLine("Make: " +Make+ ", Model: "+Model+ ", Color: "+Color+", Year: "+Year);
     }
 }
